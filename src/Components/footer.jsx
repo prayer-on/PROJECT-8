bb94b784c16bfb28916117317f3c6a69ce0ca71c
prayer-on <prayer.onaiwu@gmail.com>
2026-03-19 23:07:21 +0100

    contact section developed

--- src/Components/footer.jsx
+++ src/Components/footer.jsx
@@ -3,13 +3,13 @@
 function Footer () {
     return(
         <footer>
-            <h1>Contact me</h1>
+            <h2>Contact Me</h2>
             <form>
-                <label for= "email-adress">Email-Address:</label>
-                <input type="email" id="email-adress" name="email_contact" placeholder="frank.mult@gmail.com" required/>
+                <label for= "email-adress">Email-address:</label>
+                <input type="email" id="email-adress" name="email_contact" placeholder="john.wern@gmail.com" required/>
                 <label for="text-area">Message:</label>
                 <textarea id="text-area" name="text_message" placeholder="Write here..."></textarea>
-                <button type="submit">Send</button>
+                <button type="submit">SEND</button>
             </form>
         </footer>
     );
