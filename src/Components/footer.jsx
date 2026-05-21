517f9c927f5ad7b2cd6e80911f2364f9c7f61b9b
prayer-on <prayer.onaiwu@gmail.com>
2026-05-21 23:07:10 +0200

    fix emailjs form submission syntax

--- src/Components/footer.jsx
+++ src/Components/footer.jsx
@@ -12,10 +12,9 @@ function Footer () {
     emailjs.sendForm(
       import.meta.env.VITE_SERVICE_ID,       
        import.meta.env.VITE_TEMPLATE_ID,   
-      e.target,                
+      e.target,               
        import.meta.env.VITE_PUBLIC_KEY     
-    )
-     .then(() => {
+    ).then(() => {
     setIsSent(true);
      })
 
