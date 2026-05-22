df5a60d3d89ecd3fe6a7180966b80b40781d1b7d
prayer-on <prayer.onaiwu@gmail.com>
2026-05-22 21:05:36 +0200

    fix: fix contact form email submission and implement useRef

--- src/Components/footer.jsx
+++ src/Components/footer.jsx
@@ -1,24 +1,28 @@
 import emailjs from '@emailjs/browser';
-import {useState} from "react" 
+import {useState, useRef} from "react" 
 import ContactMessage  from "../Components/cntmessage.jsx";
 
 
 function Footer () {
     
     const [isSent, setIsSent] = useState(false)
+    const formRef = useRef();
+
 
     const messageDisplay = (e) => {
     e.preventDefault();
     emailjs.sendForm(
-      import.meta.env.VITE_SERVICE_ID,       
-       import.meta.env.VITE_TEMPLATE_ID,   
-      e.target,               
-       import.meta.env.VITE_PUBLIC_KEY     
-    ).then(() => {
+    import.meta.env.VITE_SERVICE_ID,       
+    import.meta.env.VITE_TEMPLATE_ID,   
+    formRef.current,               
+    import.meta.env.VITE_PUBLIC_KEY     
+    )
+    .then(() => {
     setIsSent(true);
      })
-
-    .catch((error) => { console.log (error)})
+    .catch((error) => { 
+    console.log (error);
+    });
     }
 
          return(
@@ -27,10 +31,10 @@ function Footer () {
             : 
             (<>
             <h2>CONTACT ME</h2>
-            <form onSubmit={messageDisplay}>
-                <label for= "email-adress">Email-address:</label>
-                <input type="email" id="email-adress" name="email_contact" placeholder="john.wern@gmail.com" required/>
-                <label for="text-area">Message:</label>
+            <form ref={formRef} onSubmit={messageDisplay}>
+                <label htmlFor= "email-address">Email-address:</label>
+                <input type="email" id="email-address" name="email_contact" placeholder="john.wern@gmail.com" required/>
+                <label htmlFor="text-area">Message:</label>
                 <textarea id="text-area" name="text_message" placeholder="Write here..." required></textarea>
                 <button type="submit">SEND</button>
             </form>
